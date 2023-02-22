44160473045a9177139cca9b8b2a97f11c6797f3
Mariia <milonigame1@gmail.com>
2023-02-22 19:47:15 +0300

    parenSelector

--- js/script.js
+++ js/script.js
@@ -163,14 +163,16 @@ window.addEventListener('DOMContentLoaded', function() {
     //Используем классы для карточек
 
     class MenuCard{
-        constructor(src, alt, title, descr, price){
+        constructor(src, alt, title, descr, price,parentSelector){
             this.src=src;
             this.alt=alt;
             this.title=title;
             this.descr= descr;
             this.price=price;
+            this.parent=document.querySelector(parentSelector);
             this.transfer=27;
             this.changeToRUB();
+        
         }
 
         changeToRUB(){
@@ -182,13 +184,16 @@ window.addEventListener('DOMContentLoaded', function() {
             <div class="menu__item">
                 <img src="img/tabs/vegy.jpg" alt="vegy">
                 <h3 class="menu__item-subtitle">Меню "Фитнес"</h3>
-                <div class="menu__item-descr">Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!</div>
+                <div class="menu__item-descr">Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. 
+                Продукт активных и здоровых людей. 
+                Это абсолютно новый продукт с оптимальной ценой и высоким качеством!</div>
                 <div class="menu__item-divider"></div>
                 <div class="menu__item-price">
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>229</span> грн/день</div>
                 </div>
             </div>`;
+            this.parent.append(element);
         }
     }
     
